c9cacc17381d4aac72a4cb93cc0a42d6eac8f35a
Muoneke Christopher <98641970+Xtopheerr@users.noreply.github.com>
2022-04-19 20:26:55 -0700

    Update print_long_upx.c

--- print_long_upx.c
+++ print_long_upx.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * prinhupx - prints a short decimal in hexadecimal
+ * prinlupx - prints a long decimal in hexadecimal
  * @arguments: The character to print
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
-int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
+int prinlupx(va_list arguments, char *buf, unsigned int ibuf)
 {
-	short int int_input, i, isnegative, count, first_digit;
+	long int int_input, i, isnegative, count, first_digit;
 	char *hexadecimal, *binary;
 
-	int_input = va_arg(arguments, int);
+	int_input = va_arg(arguments, long int);
 	isnegative = 0;
-
 	if (int_input == 0)
 	{
 		ibuf = handl_buf(buf, '0', ibuf);
@@ -26,11 +25,10 @@ int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
 		isnegative = 1;
 	}
 
-	binary = malloc(sizeof(char) * (16 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 16);
-	hexadecimal = malloc(sizeof(char) * (4 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 4);
-
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 16);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
 		if (hexadecimal[i] != '0' && first_digit == 0)
@@ -41,9 +39,7 @@ int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
 			count++;
 		}
 	}
-
 	free(binary);
 	free(hexadecimal);
-
 	return (count);
 }
